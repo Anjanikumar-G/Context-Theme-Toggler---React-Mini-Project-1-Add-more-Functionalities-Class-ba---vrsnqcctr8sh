6d452a5d506f50067aa8092a3cdff33d6325f691
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-04-22 15:47:47 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { LocalThemedBox } from './LocalThemedBox';
 import { ThemeContext } from './ThemeProvider';
 
 const Page = () => {
+    // Using context to get the global theme
+    const { theme } = useContext(ThemeContext);
 
-    const {theme,setTheme} = useContext(ThemeContext);
-
-    return(
-        <div className={'bg-'+theme} id="themed-page">
-            <p id="themed-text-container" className={'txt-'+theme}>
+    return (
+        <div className={container ${theme === 'light' ? 'bg-light' : 'bg-dark'}} id="themed-page">
+            {/* Themed Text */}
+            <p id="themed-text-container" className={txt-${theme}}>
                 lorem ipsum dolor iterit n stuff
             </p>
-            {/* 'btn btn-'+theme 'txt-'+theme */}
-            <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">Themed Button</button>
-           
-        <LocalThemedBox/>
-           
+            {/* Themed Button */}
+            <button className={btn ${theme === 'light' ? 'btn-light txt-light' : 'btn-dark txt-dark'}} id="themed-button">
+                Themed Button
+            </button>
+            {/* LocalThemedBox */}
+            <LocalThemedBox />
         </div>
-    )
-}
+    );
+};
 
-export { Page }
+export { Page };
