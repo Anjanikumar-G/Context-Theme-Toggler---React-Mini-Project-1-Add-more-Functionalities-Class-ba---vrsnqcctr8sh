81523781d13836ce662f50cc5eaa5416bd5ed7b7
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-04-22 16:25:31 +0530

    Updated Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -7,13 +7,13 @@ const Page = () => {
     const { theme } = useContext(ThemeContext);
 
     return (
-        <div className={container ${theme === 'light' ? 'bg-light' : 'bg-dark'}} id="themed-page">
+        <div className={`container ${theme === 'light' ? 'bg-light' : 'bg-dark'}`} id="themed-page">
             {/* Themed Text */}
-            <p id="themed-text-container" className={txt-${theme}}>
+            <p id="themed-text-container" className={`txt-${theme}`}>
                 lorem ipsum dolor iterit n stuff
             </p>
             {/* Themed Button */}
-            <button className={btn ${theme === 'light' ? 'btn-light txt-light' : 'btn-dark txt-dark'}} id="themed-button">
+            <button className={`btn ${theme === 'light' ? 'btn-light txt-light' : 'btn-dark txt-dark'}`} id="themed-button">
                 Themed Button
             </button>
             {/* LocalThemedBox */}
